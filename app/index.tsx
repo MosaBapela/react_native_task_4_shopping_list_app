c69fbea43b11967ce337ccea239837c84f8aa09c
MosaBapela <charitybapela2003@gmail.com>
2025-12-01 22:29:50 +0200

    fixing problems

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import AppNavigator from './navigation/AppNavigator';
+import { persistor, store } from './redux/store';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Provider store={store}>
+      <PersistGate 
+        loading={
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <ActivityIndicator size="large" color="#2563eb" />
+          </View>
+        } 
+        persistor={persistor}
+      >
+        <AppNavigator />
+      </PersistGate>
+    </Provider>
   );
 }
